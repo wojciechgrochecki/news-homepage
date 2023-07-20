4530c7c76e89a4f20a956296800d75ca9bd21723
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-20 22:52:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className=" font-normal">Hello world!</h1>
+      <h1 className=" font-bold">Hello world!</h1>
+      <h1 className=" font-bolder">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
