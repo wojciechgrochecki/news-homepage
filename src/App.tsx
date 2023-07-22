5f81f3a372a5f8791c4fcbdc732449094ff31119
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-22 11:59:32 +0200

    add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ function App() {
     <div className=" mx-auto max-w-[1440px] bg-neutral-off-white px-4 py-3">
       <Navigation />
       <Content />
+      <div className=" mt-10 hidden text-center text-xl xl:block ">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a className=" whitespace-nowrap text-primary-soft-red " href="#">
+          Wojciech Grochecki
+        </a>
+        .
+      </div>
     </div>
   );
 }
